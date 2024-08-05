566d3186d8e65d6ca9532584f82d7f3570d2832f
lucia <thisislucia1991@gmail.com>
2024-08-05 13:43:40 +0800

    [FIX]:fix typescript compile problem in useCanvasStore.ts

--- stores/useCanvasStore.ts
+++ stores/useCanvasStore.ts
@@ -10,8 +10,6 @@ interface CanvasState {
   setGrid: (grid: fabric.Group) => void;
   setZoom: (zoom: number) => void;
   setPan: (pan: { x: number; y: number }) => void;
-  updateGridPosition: () => void;
-  recreateGrid: () => void;
 }
 
 const useCanvasStore = create<CanvasState>((set, get) => ({
@@ -23,34 +21,6 @@ const useCanvasStore = create<CanvasState>((set, get) => ({
   setGrid: (grid) => set({ grid }),
   setZoom: (zoom) => set({ zoom }),
   setPan: (pan) => set({ pan }),
-  //更新網格線在畫布的位置，使其居中
-  updateGridPosition: () => {
-    const { canvas, grid, zoom } = get();
-    if (canvas && grid) {
-      const vpt = canvas.viewportTransform;
-      if (vpt) {
-        grid.set({
-          left: (canvas.getWidth() / 2 - vpt[4]) / zoom,
-          top: (canvas.getHeight() / 2 - vpt[5]) / zoom,
-        });
-        grid.setCoords();
-        canvas.requestRenderAll();
-      }
-    }
-  },
-  //重新創建網格線(使用在視窗縮放時)
-  recreateGrid: () => {
-    const { canvas, grid } = get();
-    if (canvas) {
-      if (grid) {
-        canvas.remove(grid);
-      }
-      const newGrid = createGrid(canvas.getWidth(), canvas.getHeight());
-      canvas.add(newGrid);
-      set({ grid: newGrid });
-      get().updateGridPosition();
-    }
-  },
 }));
 
 export default useCanvasStore;
