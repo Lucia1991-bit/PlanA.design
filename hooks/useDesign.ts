a9827a212af25be9fb216f26b0d9149cc3dbec01
lucia <thisislucia1991@gmail.com>
2024-09-06 04:43:48 +0800

    [REFACTOR]:Testing wall drawing functionality to fix some issue

--- hooks/useDesign.ts
+++ hooks/useDesign.ts
@@ -18,7 +18,7 @@ import { useHotkeys } from "@/hooks/useHotkeys";
 import { useClipboard } from "@/hooks/useClipboard";
 import { useHistory } from "./useHistory";
 import { useLoadDesign } from "./useLoadDesign";
-import { useDrawWall } from "./useDrawWall";
+import { useDrawWall } from "./useDrawWall2";
 import { usePattern } from "./usePattern";
 import { useContextMenu } from "./useContextMenu";
 import { useCanvasOrdering } from "./useCanvasOrdering";
